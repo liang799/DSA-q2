57e2e0c8b8145465aec4fb3d1818d48646ecbc3b
Dryices <87692678+Dryices@users.noreply.github.com>
2021-07-31 21:50:49 +0800

    Assigning sports vacancy and symbol to variables in a class. Still no storage system. Unsure of how to read in Special :G Unsure of how to iniatalize array with corresponding vacancy and symbol.

--- DSA-q2/DSA-q2.cpp
+++ DSA-q2/DSA-q2.cpp
@@ -4,22 +4,50 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
+#include <vector>
 
 using namespace std;
 
+  
+
+
+class Sport {
+public:
+    string name;
+    string symbol;
+    int vacancy;
+    
+};
+
 int main()
 {
+    Sport asport;
     ifstream read;
-    string line;
+    string line; //to hold one row of string from vac file
     read.open("vac.txt");
-    
+   
+    char letter, vac;
+    int actvac=0, i = 0;
+
     //read vacancy file
-    while (read) {
+ 
+    while (read) 
+    {
         getline(read, line);
-        cout << line << endl;
+        letter = line.front();
+        vac = line.back();
+        actvac = vac - '0'; //Convert space from char to int, the ASCII values of the characters are subtracted from each other
+        
+        asport.symbol = letter;
+        asport.vacancy = actvac;
+
+        cout << asport.symbol;
+        cout << asport.vacancy << endl;
+
     }
     read.close();
-
+    
     return 0;
 }
 
