7123c0ac2640234493212e057cbcfa323c4dbbea
liang799 <71926348+liang799@users.noreply.github.com>
2021-08-05 22:25:12 +0800

    Round 1 is fully working!

--- DSA-q2/DSA-q2.cpp
+++ DSA-q2/DSA-q2.cpp
@@ -14,13 +14,13 @@ void printResults(int round, vector<SpStudent> winners, vector<SpStudent> nowin,
     cout << "================== " << endl;
     vector<SpStudent>::iterator itr;
     for (vector<SpStudent>::iterator it = winners.begin(); it != winners.end(); it++)
-        cout <<(*it).name<<" = "<<(*it).allocated << endl;
-    for (vector<SpStudent>::iterator itr = nowin.begin(); itr!= nowin.end(); itr++)
-        cout <<(*itr).name<<" = "<<(*itr).allocated << endl;
-    cout << "================== " << endl;
-    cout << " Leftover Vacancy " << endl;
-    for (vector<Sport>::iterator i = sports.begin(); i != sports.end(); i++)
-        cout << (*i).leftover_vacancy << endl;
+		cout << (*it).name << " = " << (*it).allocated << endl;
+	for (vector<SpStudent>::iterator itr = nowin.begin(); itr != nowin.end(); itr++)
+		cout << (*itr).name << " = " << (*itr).allocated << endl;
+	cout << "================== " << endl;
+	cout << " Leftover Vacancy " << endl;
+	for (vector<Sport>::iterator i = sports.begin(); i != sports.end(); i++)
+		cout << (*i).leftover_vacancy << endl;
 }
 
 int main()
@@ -28,34 +28,33 @@ int main()
     ifstream read("vac.txt");
     string line; //to hold one row of string from vac file
     //string name;
-   
+
     char letter, vac;
     int actvac = 0;
     vector<Sport> sports;
-	Sport asport;
+    Sport asport;
     char special;
 
     //read vacancy file
     while (!read.eof()) {
-
         getline(read, line); //Copy read to line
 
         //int find = line.find( "(" ); // I suggest we remove this line as special don't have "("
         //name = line.substr(0, find); //Find the name of the sport and assign to string variable name
-      
+
         letter = line.front(); //Get symbol
         vac = line.back(); //Get vacancy
-        if (line.back() > 'A' && line.back() < 'Z') { 
+        if (line.back() > 'A' && line.back() < 'Z') {
             special = letter;
         } else {
-			actvac = vac - '0'; //Convert vacancy from char to int, the ASCII values of the characters are subtracted from each other
-			
-			//asport.name = name;
-			asport.symbol = letter; 
-			asport.vacancy = actvac;
-			asport.leftover_vacancy = actvac;
+            actvac = vac - '0'; //Convert vacancy from char to int, the ASCII values of the characters are subtracted from each other
 
-			sports.push_back(asport);
+            //asport.name = name;
+            asport.symbol = letter;
+            asport.vacancy = actvac;
+            asport.leftover_vacancy = actvac;
+
+            sports.push_back(asport);
         }
     }
     read.close();
@@ -66,7 +65,7 @@ int main()
     vector<SpStudent> stud;
 
     while (!read.eof()) {
-        getline(read, aStudent.name);   
+        getline(read, aStudent.name);
         read >> aStudent.gpa;
         getline(read, line); // getline is needed to remove the line
         read >> aStudent.choices[0] >> aStudent.choices[1] >> aStudent.choices[2];
@@ -79,38 +78,45 @@ int main()
 
     /* -------------------Round 1 ----------------------------------*/
     vector<SpStudent>::iterator it;
-    vector<Sport>::iterator i=sports.begin();
-    vector<SpStudent> winner = stud;   // first choice with winning
+    vector<Sport>::iterator i;
     vector<SpStudent> noWinning; // first choice but no winning record
-    vector<std::string>::size_type tmp;
-    for (it=stud.begin(), tmp = 0; it != stud.end(); it++, tmp++)
-    {  
+    for (it = stud.begin(); it != stud.end(); it++) {
         i = sports.begin();
-        while (i != sports.end())
-        {
+
+        for (i = sports.begin(); i != sports.end(); i++){
             //find the sport that is special set the steal attempts
             if ((*i).symbol == special)
                 (*i).steal = 3;
 
             //prioritize students with winning record
-            if ((*it).choices[0] == (*i).symbol && (*i).leftover_vacancy > 0 )
-            {
+            if ((*it).choices[0] == (*i).symbol && (*i).leftover_vacancy > 0) {
                 if ((*it).win[0] == true) {
                     (*it).allocated = (*i).symbol;
                     (*i).leftover_vacancy--;
                     break;
-                }
-                else {
+                } else {
                     noWinning.push_back(*it); //add to vector
-                    winner.erase(winner.begin() + tmp);
-                    tmp--;
                     break;
                 }
-            } 
-            else
-                i++;
+            }
         }
     }
+
+    // removing all the non winners
+    vector<SpStudent> winner = stud;   // first choice with winning
+    int tmp;
+    for (it = stud.begin(), tmp = 0; it != stud.end(); it++, tmp++) {
+        i = sports.begin();
+        for (i = sports.begin(); i != sports.end(); i++) {
+            if ( (*it).win[0] == 0 ) {
+                winner.erase(winner.begin() + tmp);
+                tmp--; // need to decrement as the size of winner got smaller by 1
+                break;
+            } else
+                break;
+        }
+    }
+
     // vector of people with first choice, but no winning record
     // gpa and sports choice are all over the place.
     // sort according to gpa. the higher gpa go first, get choice
@@ -124,8 +130,7 @@ int main()
 	i = sports.begin();
     for (vector<SpStudent>::iterator itr = noWinning.begin(); itr != noWinning.end(); itr++) {
         i = sports.begin();
-        while (i != sports.end())
-        {
+        while (i != sports.end()) {
             //prioritize students with winning record
             if ((*itr).choices[0] == (*i).symbol && (*i).leftover_vacancy > 0 ) {
 				(*itr).allocated = (*i).symbol;
